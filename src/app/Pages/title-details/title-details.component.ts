4916f667229bc809b972ec955742d55b9d525c24
Karrtik12 <kartik.chat1212@gmail.com>
2024-07-19 14:42:08 +0530

    Enhance title details component with UI improvements

--- src/app/Pages/title-details/title-details.component.ts
+++ src/app/Pages/title-details/title-details.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { NgIf, NgFor } from '@angular/common'
 import { TitlesService } from '../../Services/titles.service';
 import { TitleDetailModel } from '../../Models/title';
 import { LoadingBarComponent } from "../../Elements/loading-bar/loading-bar.component";
+import { AuthService } from '../../Services/auth.service';
 
 @Component({
   selector: 'app-title-detail',
@@ -14,14 +15,43 @@ import { LoadingBarComponent } from "../../Elements/loading-bar/loading-bar.comp
 })
 export class TitleDetailsComponent implements OnInit {
   title: TitleDetailModel | null = null;
+  authService = inject(AuthService)
   loading: boolean = false;
   titleService = inject(TitlesService)
 
   constructor(
-    private route: ActivatedRoute
+    private route: ActivatedRoute, private router: Router
   ) { }
 
   ngOnInit(): void {
+    if (
+      localStorage.getItem('loggedInUserEmail') !== undefined &&
+      localStorage.getItem('loggedInUserEmail') !== null &&
+      localStorage.getItem('loggedInUserEmail') !== ''
+    ) {
+      this.authService.loggedInUser.email =
+        localStorage.getItem('loggedInUserEmail')!;
+    }
+    if (
+      localStorage.getItem('loggedInUserAge') !== undefined &&
+      localStorage.getItem('loggedInUserEmail') !== null &&
+      localStorage.getItem('loggedInUserEmail') !== '0'
+    ) {
+      this.authService.loggedInUser.age = parseInt(
+        localStorage.getItem('loggedInUserAge')!
+      );
+    }
+    localStorage.setItem(
+      'loggedInUserEmail',
+      this.authService.loggedInUser.email
+    );
+    localStorage.setItem(
+      'loggedInUserAge',
+      this.authService.loggedInUser.age.toString()
+    );
+    if (this.authService.loggedInUser.email === '') {
+      this.navigateToLogin();
+    }
     this.loading = true;
     this.route.params.subscribe(params => {
       const id = params['id'];
@@ -31,8 +61,13 @@ export class TitleDetailsComponent implements OnInit {
     });
     setTimeout(() => {
       this.loading = false;
-    }, 1000);
+    }, 2000);
   }
+
+  navigateToLogin(): void {
+    this.router.navigate(['/login']);
+  }
+
   convertToHoursOrSeason(input: string) {
     const minuteRegex = /^(\d+) min$/;
 
